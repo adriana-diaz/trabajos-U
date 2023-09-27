e5b5de3908862bdcb718040ebb8ea2090a818e64
adriana-diaz <122817007+adriana-diaz@users.noreply.github.com>
2023-09-26 18:41:01 -0600

    Update DemoApplication.java

--- Programacion III/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Programacion III/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,8 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class DemoApplication {
+        public class DemoApplication {
 
 	public static void main(String[] args) {
         class Carro{
