44ecbeb4b04cd15d355394a39e9b4b5b7168ea39
adriana-diaz <122817007+adriana-diaz@users.noreply.github.com>
2023-09-26 18:39:04 -0600

    Add files via upload

---
+++ Programacion III/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+
+       class Carro{
+
+	   }
+
+       class Autobus{
+		String marca;
+		String modelo;
+		String color;
+		public String getMarca() {return marca;}
+	   }
+
+
+
+
+
+
+			class Estudiantes {
+		String nombre;
+		String apellido;
+		String carnet;
+		private String saludar(){
+			return "Hola mi nombre es "+nombre+" y mi apellido es"+apellido+"" ;}
+	
+	}
+		System.out.println("Acelerando");
+		Estudiantes estudiant1 = new Estudiantes();
+		estudiant1.nombre = "Estudiante #1";
+		estudiant1.apellido = "Apellido #1";
+		estudiant1.carnet= "12345";
+		System.out.println("Estudiante :"+estudiant1.nombre+"apellido:"+estudiant1.apellido +" carnet:" +estudiant1.carnet);
+		System.out.println("el estudiante dice: "+estudiant1.saludar());
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
