67b810dfc80ca5e931dda481036d10091bcfd4d4
adriana-diaz <122817007+adriana-diaz@users.noreply.github.com>
2023-09-26 18:52:22 -0600

    Update DemoApplication.java

--- Programacion III/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Programacion III/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-        public class DemoApplication implements Escape {
+        public class DemoApplication  {
 
 	public static void main(String[] args) {
         class Carro{
