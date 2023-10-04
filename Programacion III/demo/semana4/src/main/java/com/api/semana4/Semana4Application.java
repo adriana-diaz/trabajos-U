8a21d8b5ed413c3c775022fe0621131a4ea0e0c8
adriana-diaz <122817007+adriana-diaz@users.noreply.github.com>
2023-10-03 20:04:43 -0600

    semana 4-api

---
+++ Programacion III/demo/semana4/src/main/java/com/api/semana4/Semana4Application.java
@@ -0,0 +1,13 @@
+package com.api.semana4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana4Application.class, args);
+	}
+
+}
---
+++ Programacion III/demo/semana4/src/test/java/com/api/semana4/Semana4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.semana4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
