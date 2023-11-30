c69cefcf983b8d194cdef9f343c494bfa5956477
Adriana Diaz <122817007+adriana-diaz@users.noreply.github.com>
2023-11-30 15:47:36 -0600

    Update ProyectoAvance.sql

--- BISI06 Bases de Datos/ProyectoAvance.sql
+++ BISI06 Bases de Datos/ProyectoAvance.sql
@@ -5,41 +5,43 @@ CREATE DATABASE IF NOT EXISTS INVENTARIO_ELECTROTIENDA;
 USE INVENTARIO_ELECTROTIENDA;
 
 
-/**TABLAS**/
+/**TABLAS revisadas**/
+CREATE TABLE Categoria (
+    IDCategoria INT,
+    Nombre VARCHAR(50),
+    Descripcion VARCHAR(100) NOT NULL,
+	ID_Codigo_Producto INT,
+    CONSTRAINT PK_Categoria PRIMARY KEY(IDCategoria)
+);
+CREATE TABLE Proveedor (
+    IDProveedor INT,
+    Nombre VARCHAR(50),
+    Direccion VARCHAR(100) NOT NULL,
+    NumeroDeTelefono VARCHAR(50),
+    ID_Codigo_Producto INT,
+    CONSTRAINT PK_Proveedor PRIMARY KEY(IDProveedor)
+);
+CREATE TABLE Promociones (
+    IDPromociones INT,
+    Descuento VARCHAR(50),
+    Fecha_de_inicio VARCHAR(100) NOT NULL,
+    Fecha_de_finalizacion VARCHAR(50),
+    ID_Codigo_Producto INT,
+    CONSTRAINT PK_Promociones PRIMARY KEY(IDPromociones)
+);
 
 CREATE TABLE Producto (
     ID_Codigo_Producto INT PRIMARY KEY,
     Nombre VARCHAR(50),
     Descripcion VARCHAR(100) NOT NULL,
-    Precio DECIMAL(10, 2) NOT NULL, -- Cambiado a DECIMAL para almacenar valores de precios
-    Cantidad_en_stock INT NOT NULL, -- Cambiado a INT para almacenar valores de cantidad como números enteros
-    IDCategoria INT, -- Necesitas declarar esta columna antes de usarla en la FOREIGN KEY
-    IDProveedor INT, -- Necesitas declarar esta columna antes de usarla en la FOREIGN KEY
-    IDPromociones INT, -- Necesitas declarar esta columna antes de usarla en la FOREIGN KEY
-    FOREIGN KEY (IDCategoria) REFERENCES Categoria(IDCategoria),
-    FOREIGN KEY (IDProveedor) REFERENCES Proveedor(IDProveedor),
-    FOREIGN KEY (IDPromociones) REFERENCES Promociones(IDPromociones)
+    Precio DECIMAL(10, 2) NOT NULL,
+    Cantidad_en_stock INT NOT NULL,
+    IDCategoria INT,
+    IDProveedor INT,
+    IDPromociones INT,
+    CONSTRAINT FK_Producto_Categoria FOREIGN KEY (IDCategoria) REFERENCES Categoria(IDCategoria),
+    CONSTRAINT FK_Producto_Proveedor FOREIGN KEY (IDProveedor) REFERENCES Proveedor(IDProveedor),
+    CONSTRAINT FK_Producto_Promociones FOREIGN KEY (IDPromociones) REFERENCES Promociones(IDPromociones)
 );
-CREATE TABLE Categoria (
-    IDCategoria INT PRIMARY KEY,
-    Nombre VARCHAR(50),
-    Descripcion VARCHAR(100) NOT NULL,
-	ID_Codigo_Producto INT,
-    FOREIGN KEY (ID_Codigo_Producto) REFERENCES Producto(ID_Codigo_Producto)
-);
-CREATE TABLE Proveedor (
-    IDProveedor INT PRIMARY KEY,
-    Nombre VARCHAR(50),
-    Direccion VARCHAR(100) NOT NULL,
-    NumeroDeTelefono VARCHAR(50),
-    ID_Codigo_Producto INT,
-    FOREIGN KEY (ID_Codigo_Producto) REFERENCES Producto(ID_Codigo_Producto)
-);
-CREATE TABLE Promociones (
-    IDPromociones INT PRIMARY KEY,
-    Descuento VARCHAR(50),
-    Fecha_de_inicio VARCHAR(100) NOT NULL,
-    Fecha_de_finalizacion VARCHAR(50),
-    ID_Codigo_Producto INT,
-    FOREIGN KEY (ID_Codigo_Producto) REFERENCES Producto(ID_Codigo_Producto)
-);
+
+
