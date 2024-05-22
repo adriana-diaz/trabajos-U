10daa45efb39eed7437cc108f92aeac442f8cb38
Adriana Diaz <122817007+adriana-diaz@users.noreply.github.com>
2024-05-22 14:46:08 -0600

    Add files via upload

---
+++ BISI05 Programacion Web/Inicializacion a Plataforma/prograweb/Backend/Logica/Entidades/Request/ReqIngresarUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Logica.Entidades
+{
+	public class ReqIngresarUsuario
+	{
+		public Usuario usuario {  get; set; }
+	}
+}
