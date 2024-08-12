3e6ee7ad67767d140dbd6bb37b61d09f0b884a47
Adriana Diaz <122817007+adriana-diaz@users.noreply.github.com>
2024-08-12 14:31:23 -0600

    Update Selects y pruebas proyectoweb.sql

--- BISI05 Programacion Web/Semana 7 HTML/Selects y pruebas proyectoweb.sql
+++ BISI05 Programacion Web/Semana 7 HTML/Selects y pruebas proyectoweb.sql
@@ -1,6 +1,6 @@
 --NO TOCAR
 --Pruebas
---Agregar usuario
+--Agregar usuario LISTO!!!!!!!!
 USE BDProyectoWeb
 GO
 DECLARE @IDRETURN INT;
@@ -8,10 +8,10 @@ DECLARE @ERRORID INT;
 DECLARE @ERRORDESCRIPCION NVARCHAR(MAX);
 
 EXEC SP_INGRESAR_USUARIO
-    @CEDULA = 559595955,          -- Cédula del usuario (debe ser única)
-    @NOMBRE = 'daniel',
-    @EMAIL = 'dani@gmail.com',
-    @PASSWORD = 'securepassword',
+    @CEDULA = 1194256555,          -- CÃ©dula del usuario (debe ser Ãºnica)
+    @NOMBRE = 'fito diaz',
+    @EMAIL = 'fito@gmail.com',
+    @PASSWORD = 'fitolol',
     @IDRETURN = @IDRETURN OUTPUT,
     @ERRORID = @ERRORID OUTPUT,
     @ERRORDESCRIPCION = @ERRORDESCRIPCION OUTPUT;
@@ -20,38 +20,38 @@ SELECT @IDRETURN AS IDRETURN, @ERRORID AS ERRORID, @ERRORDESCRIPCION AS ERRORDES
 
 
 ------------------------------------------------------
---ELIMINAR USUARIO
+--ELIMINAR USUARIO LISTO!!!!!!!!
 DECLARE @IDRETURN INT;
 DECLARE @ERRORID INT;
 DECLARE @ERRORDESCRIPCION NVARCHAR(MAX);
 EXEC SP_ELIMINAR_USUARIO
-    @CEDULA = 559595955,   
+    @CEDULA = 1194256555,   
     @IDRETURN = @IDRETURN OUTPUT,
     @ERRORID = @ERRORID OUTPUT,
     @ERRORDESCRIPCION = @ERRORDESCRIPCION OUTPUT;
 SELECT @IDRETURN AS IDRETURN, @ERRORID AS ERRORID, @ERRORDESCRIPCION AS ERRORDESCRIPCION;
 
 --------------------------------------------------
---Actualizar Usuario
+--Actualizar Usuario LISTO!!!!!!!!
 DECLARE @IDRETURN INT;
 DECLARE @ERRORID INT;
 DECLARE @ERRORDESCRIPCION NVARCHAR(MAX);
 EXEC SP_ACTUALIZAR_USUARIO
-    @CEDULA = 559595955,          -- Cédula del usuario
-    @NOMBRE = 'daniel',
-    @EMAIL = 'dani@gmail.com',
+    @CEDULA = 119320150,          -- CÃ©dula del usuario
+    @NOMBRE = 'adri diaz 2',
+    @EMAIL = 'adri@gmail.com',
     @PASSWORD = 'newsecurepassword',
     @IDRETURN = @IDRETURN OUTPUT,
     @ERRORID = @ERRORID OUTPUT,
     @ERRORDESCRIPCION = @ERRORDESCRIPCION OUTPUT;
 SELECT @IDRETURN AS IDRETURN, @ERRORID AS ERRORID, @ERRORDESCRIPCION AS ERRORDESCRIPCION;
 -------------------------------------------------------------------------------------------
---INICIAR SESION
+--INICIAR SESION LISTO!!!!!!!!
 DECLARE @SESION_ID BIGINT;
 DECLARE @ERRORID INT;
 DECLARE @ERRORDESCRIPCION NVARCHAR(MAX);
 EXEC SP_LOGIN_USUARIO
-    @EMAIL = 'adriupdated@gmail.com',
+    @EMAIL = 'adri@gmail.com',
     @PASSWORD = 'newsecurepassword',
     @SESION_ID = @SESION_ID OUTPUT,
     @ERRORID = @ERRORID OUTPUT,
@@ -60,65 +60,87 @@ SELECT @SESION_ID AS SESION_ID, @ERRORID AS ERRORID, @ERRORDESCRIPCION AS ERRORD
 
 
 -------------------------------------------------------------
---CERRAR SESION // debria de ser por usuario
+--CERRAR SESION // debria de ser por usuario LISTO!!!!!!!!
 DECLARE @IDRETURN INT;
 DECLARE @ERRORID INT;
 DECLARE @ERRORDESCRIPCION NVARCHAR(MAX);
 
 EXEC SP_CERRAR_SESION
-    @SESION_ID = 9,          -- ID de la sesión que se desea cerrar
+    @SESION_ID = 1,          -- ID de la sesiÃ³n que se desea cerrar
     @IDRETURN = @IDRETURN OUTPUT,
     @ERRORID = @ERRORID OUTPUT,
     @ERRORDESCRIPCION = @ERRORDESCRIPCION OUTPUT;
 
 SELECT @IDRETURN AS IDRETURN, @ERRORID AS ERRORID, @ERRORDESCRIPCION AS ERRORDESCRIPCION;
 -------------------------------------------------------------------------------------
---AGREGAR PRODUCTO
+--INSERTS CATEGORIAS
+--Insertar una categorÃ­a de ejemplo
+INSERT INTO Categorias (nombre, descripcion) 
+VALUES ('Prueba', 'Todas las bebidas disponibles');
+
+--AGREGAR PRODUCTO LISTO!!!!!!!!
+-- Declarar variables de salida
 DECLARE @IDRETURN INT;
 DECLARE @ERRORID INT;
 DECLARE @ERRORDESCRIPCION NVARCHAR(MAX);
 
-EXEC SP_AGREGAR_PRODUCTO 
-    @NOMBRE = 'Latte', 
-    @DESCRIPCION = 'de alta calidad de Argentina', 
-    @PRECIO = 6.66, 
-    @NOMBRE_CATEGORIA = 'Bebidas',
+-- Llamar al procedimiento almacenado
+EXEC SP_AGREGAR_PRODUCTO
+    @nombre_categoria = 'Maquinas',  -- Nombre de la categorÃ­a existente
+    @nombre = 'Choreador',
+    @descripcion = 'Alta calidad',
+    @precio_producto = 7500,
+    @cantidad = 70,
     @IDRETURN = @IDRETURN OUTPUT,
     @ERRORID = @ERRORID OUTPUT,
     @ERRORDESCRIPCION = @ERRORDESCRIPCION OUTPUT;
 
-SELECT @IDRETURN AS ID, @ERRORID AS ErrorID, @ERRORDESCRIPCION AS ErrorDescripcion;
+-- Verificar el resultado
+SELECT @IDRETURN AS IDRETURN, @ERRORID AS ERRORID, @ERRORDESCRIPCION AS ERRORDESCRIPCION;
+
+SELECT * FROM Productos;
+
 ---------------------------------------------------------------------------------------
 --eliminar PRODUCTO
-DECLARE @IDRETURN INT, @ERRORID INT, @ERRORDESCRIPCION NVARCHAR(MAX);
+-- Declarar variables de salida
+DECLARE @IDRETURN INT;
+DECLARE @ERRORID INT;
+DECLARE @ERRORDESCRIPCION NVARCHAR(MAX);
 
+-- Llamar al procedimiento almacenado
 EXEC SP_ELIMINAR_PRODUCTO
-    @NOMBRE = 'Latte postman',
+    @nombre = 'Choreador',  -- Nombre del producto a eliminar
     @IDRETURN = @IDRETURN OUTPUT,
     @ERRORID = @ERRORID OUTPUT,
     @ERRORDESCRIPCION = @ERRORDESCRIPCION OUTPUT;
 
 -- Verificar el resultado
-SELECT @IDRETURN AS Resultado, @ERRORID AS CodigoError, @ERRORDESCRIPCION AS DescripcionError;
+SELECT @IDRETURN AS IDRETURN, @ERRORID AS ERRORID, @ERRORDESCRIPCION AS ERRORDESCRIPCION;
 
+SELECT * FROM Productos;
 -------------------------------------------------------------------------------------------------
 --ACTUALIZAR PRODUCTO
--- Declarar variables de salida
-DECLARE @IDRETURN INT, @ERRORID INT, @ERRORDESCRIPCION NVARCHAR(MAX);
+DECLARE @IDRETURN INT;
+DECLARE @ERRORID INT;
+DECLARE @ERRORDESCRIPCION NVARCHAR(MAX);
 
+-- Llamar al procedimiento almacenado
 EXEC SP_ACTUALIZAR_PRODUCTO
-    @NOMBRE_ORIGINAL = 'Latte',
-    @NUEVO_NOMBRE = 'latte BD',
-    @NUEVA_DESCRIPCION = 'BD granos',
-    @NUEVO_PRECIO = 30.00,
-    @NUEVO_NOMBRE_CATEGORIA = 'Bebidas',
+    @nombre_actual = 'Expresso',  -- Nombre del producto a actualizar
+    @nuevo_nombre = 'Expresso Doble',   -- Nuevo nombre del producto
+    @descripcion = 'un mejor expresso.',
+    @precio_producto = 2600,
+    @cantidad = 15,
+    @nombre_categoria = 'Bebidas',  -- Nueva categorÃ­a del producto
     @IDRETURN = @IDRETURN OUTPUT,
     @ERRORID = @ERRORID OUTPUT,
     @ERRORDESCRIPCION = @ERRORDESCRIPCION OUTPUT;
 
 -- Verificar el resultado
-SELECT @IDRETURN AS Resultado, @ERRORID AS CodigoError, @ERRORDESCRIPCION AS DescripcionError;
+SELECT @IDRETURN AS IDRETURN, @ERRORID AS ERRORID, @ERRORDESCRIPCION AS ERRORDESCRIPCION;
 
+
+SELECT * FROM Productos;
 	------------------------------------------------------------------------------
 --Actualizar Categoria
 DECLARE @IDRETURN INT, @ERRORID INT, @ERRORDESCRIPCION NVARCHAR(MAX);
@@ -133,13 +155,17 @@ EXEC ActualizarCategoria
 
 -- Verificar el resultado
 SELECT @IDRETURN, @ERRORID, @ERRORDESCRIPCION;
+
+SELECT * FROM Categorias;
+
+
 --Eliminar categoria 
 DECLARE @IDRETURN INT;
 DECLARE @ERRORID INT;
 DECLARE @ERRORDESCRIPCION NVARCHAR(MAX);
 
 EXEC SP_ELIMINAR_CATEGORIA
-    @NOMBRE = 'Bebidas Postman',  -- Nombre de la categoría que deseas eliminar
+    @NOMBRE = 'Prueba',  -- Nombre de la categorÃ­a que deseas eliminar
     @IDRETURN = @IDRETURN OUTPUT,
     @ERRORID = @ERRORID OUTPUT,
     @ERRORDESCRIPCION = @ERRORDESCRIPCION OUTPUT;
@@ -214,20 +240,31 @@ GO
 --------------------------------------------------------------------------------
 --Compras
 -- Ejecutar el procedimiento SP_INSERTAR_COMPRA
+-- Declarar las variables necesarias
+---------------------------------------------------
+--detalleCompra
+-- Declarar las variables necesarias
+DECLARE @sesion NVARCHAR(MAX) = '93868743-D0DC-4510-A2DC-6CFC47BF92A9';  -- Reemplaza con el identificador de sesiÃ³n vÃ¡lido
+DECLARE @nombre_producto NVARCHAR(100) = 'Latte';
+DECLARE @cantidad_producto INT = 2;
+DECLARE @precio_producto DECIMAL(18, 2) = 100.50;
+DECLARE @total_compra DECIMAL(10, 2) = 201.00;
+DECLARE @fecha_compra DATETIME = GETDATE();
+ 
+-- Ejecutar el procedimiento almacenado y obtener el nombre del usuario
+EXEC InsertarDetalleCompra 
+    @sesion = @sesion,
+    @nombre_producto = @nombre_producto,
+    @cantidad_producto = @cantidad_producto,
+    @precio_producto = @precio_producto,
+    @total_compra = @total_compra,
+    @fecha_compra = @fecha_compra;
 
-------------------------------------insert-----------------------------------------
---Insertar una categoría de ejemplo
-INSERT INTO Categorias (nombre, descripcion) 
-VALUES ('Bebidas', 'Todas las bebidas disponibles');
 
-DECLARE @IDRETURN INT, @ERRORID INT, @ERRORDESCRIPCION NVARCHAR(MAX);
-EXEC SP_INSERTAR_CATEGORIA 'Maquinas', 'Descripción de la categoría', @IDRETURN OUTPUT, @ERRORID OUTPUT, @ERRORDESCRIPCION OUTPUT;
-SELECT @IDRETURN AS IDRETURN, @ERRORID AS ERRORID, @ERRORDESCRIPCION AS ERRORDESCRIPCION;
-GO
 ------------------------------------SELECTS-----------------------------------------
 SELECT * FROM Sesiones;
 SELECT * FROM Usuarios;
 SELECT * FROM Productos;
 SELECT * FROM Categorias;
 SELECT * FROM Inventario;
-
+SELECT * FROM Factura;
