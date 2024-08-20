9da0be0be12ec478db0c246ba5807c1f9ee7a80d
Adriana Diaz <122817007+adriana-diaz@users.noreply.github.com>
2024-08-20 16:38:49 -0600

    Update TablasProyecto.sql

--- BISI05 Programacion Web/Semana 7 HTML/TablasProyecto.sql
+++ BISI05 Programacion Web/Semana 7 HTML/TablasProyecto.sql
@@ -13,13 +13,12 @@ CREATE TABLE Productos (
     descripcion NVARCHAR(MAX),
     precio_producto DECIMAL(18, 2) NOT NULL,
     fecha_agregado DATETIME DEFAULT GETDATE() NOT NULL,
-	cantidad INT NULL,
     FOREIGN KEY (id_categoria) REFERENCES Categorias(id_categoria)
 );
 
 CREATE TABLE Usuarios (
     id_usuario INT IDENTITY(1,1) PRIMARY KEY,
-    cedula INT NOT NULL UNIQUE,
+    cedula NVARCHAR(20) NOT NULL UNIQUE,
     nombre NVARCHAR(100) NOT NULL,
     email NVARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -40,6 +39,8 @@ CREATE TABLE Sesiones (
 
 CREATE TABLE Carrito (
     id_carrito INT IDENTITY(1,1) PRIMARY KEY,
+	cantidad INT not null,
+	--
 	id_usuario INT NOT NULL,
 	id_producto INT NOT NULL,
 	FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
@@ -49,52 +50,19 @@ CREATE TABLE Carrito (
 
 CREATE TABLE Tarjetas (
     id_tarjeta INT IDENTITY(1,1) PRIMARY KEY,
-    numero_tarjeta INT NOT NULL UNIQUE, 
-    fecha_expiracion NVARCHAR(MAX) NOT NULL,
-	CVV INT NOT NULL UNIQUE,
+    numero_tarjeta NVARCHAR(16) NOT NULL UNIQUE, 
+    fecha_expiracion datetime NOT NULL,
+	CVV NVARCHAR(5) NOT NULL UNIQUE,
 	id_usuario INT NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
 );
 
-CREATE TABLE Compra (
-    id_compra INT IDENTITY(1,1) PRIMARY KEY,  
-    fecha DATETIME DEFAULT GETDATE() NOT NULL,
-    precio_total DECIMAL(18, 2) NOT NULL,
-	id_usuario INT NOT NULL,
-	id_producto INT NOT NULL,
-	id_tarjeta INT NOT NULL,
-	--foreign keys
-	FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
-    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto),
-	FOREIGN KEY (id_tarjeta) REFERENCES Tarjetas(id_tarjeta)
-);
-
-CREATE TABLE EncabezadoFactura (
-   id_encabezadoFactura INT IDENTITY(1,1) PRIMARY KEY, 
-   --
-   id_usuario INT NOT NULL,
-   id_compra INT NOT NULL,
-   --
-   FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
-   FOREIGN KEY (id_compra) REFERENCES Compra(id_compra),
-);
-
-CREATE TABLE DetalleFactura (
-   id_detalleFactura INT IDENTITY(1,1) PRIMARY KEY,
-   --
-   id_encabezadoFactura INT NOT NULL,
-   id_producto INT NOT NULL,
-   id_compra INT NOT NULL,
-   FOREIGN KEY (id_encabezadoFactura) REFERENCES encabezadoFactura(id_encabezadoFactura),
-   FOREIGN KEY (id_producto) REFERENCES Productos(id_producto),
-   FOREIGN KEY (id_compra) REFERENCES Compra(id_compra),
-);
-
 CREATE TABLE Factura (
    id_factura INT IDENTITY(1,1) PRIMARY KEY, 
+   fecha DATETIME DEFAULT GETDATE() NOT NULL,
    --
-   id_detalleFactura  INT NOT NULL,
-   id_encabezadoFactura  INT NOT NULL,
-   FOREIGN KEY (id_encabezadoFactura) REFERENCES EncabezadoFactura(id_encabezadoFactura),
-   FOREIGN KEY (id_detalleFactura) REFERENCES DetalleFactura(id_detalleFactura),
+   --solamente trae info del usuario
+   id_usuario INT NOT NULL,
+   --
+   FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
 );
