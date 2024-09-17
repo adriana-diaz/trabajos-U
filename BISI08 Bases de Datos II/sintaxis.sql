3cfc71d47254740a4094109ae66f9914170ecf0f
Adriana Diaz <122817007+adriana-diaz@users.noreply.github.com>
2024-09-17 13:01:19 -0600

    Create sintaxis.sql

---
+++ BISI08 Bases de Datos II/sintaxis.sql
@@ -0,0 +1,28 @@
+SELECT      # Seleccionar datos de la base de datos
+FROM        # Especificar la tabla de la que seleccionar datos
+WHERE       # Filtrar filas basadas en una condición
+AS          # Renombrar una columna o tabla con un alias
+JOIN        # Combinar filas de dos o más tablas
+AND         # Combinar múltiples condiciones, todas deben ser verdaderas
+OR          # Combinar múltiples condiciones, al menos una debe ser verdadera
+LIMIT       # Restringir el número de filas devueltas
+IN          # Especificar múltiples valores en una cláusula WHERE
+CASE        # Crear lógica condicional dentro de una sentencia SQL
+IS NULL     # Verificar valores vacíos
+LIKE        # Buscar un patrón específico en una columna
+COMMIT      # Guardar cambios realizados en la transacción
+ROLLBACK    # Deshacer los cambios realizados en la transacción
+ALTER TABLE # Modificar la estructura de una tabla existente
+UPDATE      # Modificar filas existentes en una tabla
+CREATE      # Crear una nueva tabla, vista u otro objeto en la base de datos
+DELETE      # Eliminar filas de una tabla
+INSERT INTO # Agregar nuevas filas a una tabla
+DROP        # Eliminar una tabla u otro objeto en la base de datos
+GROUP BY    # Agrupa filas que tienen los mismos valores en columnas
+ORDER BY    # Organizar el conjunto de resultados de una consulta
+HAVING      # Filtrar grupos basados en una condición
+COUNT       # Devuelve el número de filas que coinciden con un criterio
+SUM         # Calcular el total de una columna numérica
+AVG         # Calcular el valor promedio de una columna numérica
+MIN         # Devolver el valor más pequeño de la columna seleccionada
+MAX         # Devolver el valor más grande de la columna seleccionada
