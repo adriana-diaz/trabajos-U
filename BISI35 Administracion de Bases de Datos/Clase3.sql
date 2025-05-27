4114cb884a04b9925ce8c94286504a2870026247
Adriana Diaz <122817007+adriana-diaz@users.noreply.github.com>
2025-05-26 21:56:26 -0600

    Add files via upload

---
+++ BISI35 Administracion de Bases de Datos/Clase3.sql
@@ -0,0 +1,70 @@
+USE MASTER
+GO
+CREATE DATABASE Empresa 
+on Primary -- .mdf
+(
+NAME = 'EmpresaPrimary',
+filename = 'C:\ADBD\Empresa.mdf',
+size = 50MB,
+filegrowth = 15%
+)
+
+
+Log on --archivo de logs
+(
+NAME = 'EmpresaLogs',
+filename = 'C:\ADBD\EmpresaLog.ldf',
+size = 100MB,
+filegrowth = 20%
+);
+go
+
+
+
+alter database Empresa
+Add filegroup Produccion; --grupo de archivos logicos
+go
+
+--
+--create archivo adicional
+alter database Empresa
+add file
+(
+NAME = 'EmpresaProduccion',
+filename = 'C:\ADBD\EmpresaProduccion.ndf',
+size = 50MB,
+filegrowth = 20%
+)
+to filegroup Produccion;
+go
+
+--modificar la base de datos para que se use el archivo adicional, se establecce como predetermianado
+alter database Empresa
+modify filegroup [Produccion] DEFAULT;
+GO
+
+
+
+--CREATE TABLA CLIENTE
+
+CREATE TABLE dbo.Cliente (
+CodigoCliente int identity (1,1) not null primary key,
+Nombre varchar (150) not null,
+Direccion varchar (150) not null,
+Telefono varchar (20) default ('0000-0000'),
+Email varchar (40) not null,
+FechaNacimiento date not null,
+edad int not null,
+CONSTRAINT CHK_Edad Check (Edad >= 18)
+--CONSTRAINT CHK_MayoriaEdad Check (DATEDIFF (YEAR,FechaNacimiento,GETDATE())>18)
+)
+
+--insertar datos con valores invalidos, violando la restriccion check
+insert into dbo.Cliente (Nombre, Direccion, Email, FechaNacimiento, edad)
+values
+('adriana diaz', 'Heredia', 'adrianadiaz3003@gmail.com', '2005-03-30', 20);
+go
+
+
+
+
